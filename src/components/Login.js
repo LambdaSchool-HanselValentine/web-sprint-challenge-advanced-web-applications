65d771cb18744ac65c51abf0a2a117678fce54bf
HanselV <hanselviva@gmail.com>
2021-06-13 11:56:17 -0700

    login form skeleton added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
-  //replace with error state
+	const [formValues, setFormValues] = useState({ username: "", password: "" });
+	const [error, setError] = useState({
+		username: "",
+		password: "",
+	});
 
-  return (
-    <div>
-      <h1>Welcome to the Bubble App!</h1>
-      <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
-      </div>
+	const onChangeHandler = (e) => {
+		setFormValues({ ...formValues, [e.target.name]: e.target.value });
+	};
 
-      <p data-testid="errorMessage" className="error">{error}</p>
-    </div>
-  );
+	const error = "";
+	//replace with error state
+
+	return (
+		<div>
+			<h1>Welcome to the Bubble App!</h1>
+			<div data-testid="loginForm" className="login-form">
+				<h2>Login</h2>
+				<form>
+					<input
+						data-testid="username"
+						type="text"
+						name="username"
+						value={formValues.username}
+						onChange={onChangeHandler}
+						placeholder="Enter Username"
+					/>
+					<input
+						data-testid="password"
+						type="password"
+						name="password"
+						value={formValues.password}
+						onChange={onChangeHandler}
+						placeholder="Enter Password"
+					/>
+				</form>
+			</div>
+
+			<p data-testid="errorMessage" className="error">
+				{error.username}
+				{error.password}
+			</p>
+		</div>
+	);
 };
 
 export default Login;
